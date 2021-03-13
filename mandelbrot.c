70178bf60a229037c014985f445d932449644af2
JRAM0012 <jayaram0012rip@gmail.com>
2021-03-13 20:18:38 +0530

    now using shaders to draw mandlebrot set with andrdoid support

--- mandelbrot.c
+++ mandelbrot.c
@@ -2,220 +2,290 @@
 //*************************************************  RAYLIB MANDELBROT SET VISUALIZER   ***************************************************
 //********************************************************** BY JRAM0012  *****************************************************************
 //*****************************************************************************************************************************************
-
-
-
 #include "raylib.h"
 #include <stdio.h>
-#include <stdlib.h>
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
-#define REAL 1
-#define IMAG 0
-#ifndef MOVE_SPEED
-  #define MOVE_SPEED 9
+
+
+typedef struct {
+    Texture2D texture;
+    int x, y;
+}TexturedButton;
+
+#ifdef PLATFORM_ANDROID
+bool m_IsButtonPressed(TexturedButton* texturedbutton);
 #endif
 
-typedef struct ComplexNumber
+int main()
 {
-        float real;
-        float imag;
-}complex;
+#ifndef PLATFORM_ANDROID
+    int screenWidth = 1200, screenHeight = 700;
+#else
+    int screenWidth = 1544, screenHeight = 720 ;
+#endif
+    InitWindow(screenWidth, screenHeight, "mandelbort plotter v2");
+    // if (IsWindowState(FLAG_WINDOW_RESIZABLE)) ClearWindowState(FLAG_WINDOW_RESIZABLE); else SetWindowState(FLAG_WINDOW_RESIZABLE);
+    int maxIter = 256;
+    // int lastIter = maxIter;
+    char shaderCode[] = 
+"#version 100\n\
+#ifdef GL_FRAGMENT_PRECISION_HIGH\n\
+precision highp float;\n\
+#else\n\
+precision mediump float;\n\
+#endif\n\
+uniform vec2 resolution;\n\
+uniform vec2 location;\n\
+uniform float zoom;\n\
+uniform float vec4_1;\n\
+uniform float vec4_2;\n\
+uniform float vec4_3;\n\
+uniform float vec4_4;\n\
+uniform int maxIter;\n\
+vec2 compsquare(vec2 z)\n\
+{\n\
+    float temp = z.x;\n\
+    z.x = z.x * z.x - z.y * z.y;\n\
+    z.y = 2.0 * temp * z.y;\n\
+    return z;\n\
+}\n\
+vec3 hsv2rgb(vec3 hue){\n\
+    vec4 K = vec4(vec4_1, vec4_2, vec4_3, vec4_4);\n\
+    vec3 p = abs(fract(hue.xxx + K.xyz) * 6.0 - K.www);\n\
+    return hue.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), hue.y);\n\
+}\n\
+vec3 mandelbort(vec2 point){\n\
+    vec2 z = vec2(0.0);\n\
+    float iters = 0.0;\n\
+    for (iters = 0.0;   iters < float(maxIter); ++iters)\n\
+    {\n\
+        z = compsquare(z) + point;\n\
+        if(dot(z, z) > 4.0) break;\n\
+    }\n\
+    float hue = iters / float(maxIter);\n\
+    if(hue > 0.98) return vec3(0.0);\n\
+    return hsv2rgb(vec3(hue, 1.0, 1.0));\n\
+}\n\
+void main()\n\
+{\n\
+    vec2 uv = gl_FragCoord.xy / resolution;\n\
+    uv.x *= resolution.x / resolution.y;\n\
+    uv -= vec2(0.7, 0.5);\n\
+    // uv.x -= 0.5;\n\
+    uv *= zoom;\n\
+    uv -= location;\n\
+    gl_FragColor = vec4(mandelbort(uv), 1.0);\n\
+    // gl_FragColor = vec4(uv, 1.0, 1.0);\n\
+}";
 
-int MAXITER = 15;
-int iter = 0;
 
-complex maptograph(complex,int,int,int);
-complex maptoscreen(complex);
-complex compsquare(complex);
-float splitcomponents(complex,int);
-Color throwmesomeshade(int);
-int absolutevalue(complex);
-complex compadd(complex,complex);
-int compabs(complex);
-Color mandlebrot(int, int, int, int,int,int);
+    Shader shader = LoadShaderCode(0, shaderCode);
+    RenderTexture target = LoadRenderTexture(screenWidth, screenHeight);
+    int resolutionLoc    = GetShaderLocation(shader, "resolution");
+    int locationLoc      = GetShaderLocation(shader, "location");
+    int zoomLoc          = GetShaderLocation(shader, "zoom");
+    int vec4_1Loc        = GetShaderLocation(shader, "vec4_1");
+    int vec4_2Loc        = GetShaderLocation(shader, "vec4_2");
+    int vec4_3Loc        = GetShaderLocation(shader, "vec4_3");
+    int vec4_4Loc        = GetShaderLocation(shader, "vec4_4");
+    int maxiterLoc       = GetShaderLocation(shader, "maxIter");
 
-int main(int argc, char* argv[]) {
+    float res[2] = {screenWidth, screenHeight};
+    float location[2] = {0.0, 0.0};
+    float zoom = 2.0;
 
-        SetConfigFlags(FLAG_MSAA_4X_HINT);
+    float vec4_1 = 1.0f;
+    float vec4_2 = 2.0f / 3.0f;
+    float vec4_3 = 1.0f / 3.0f;
+    float vec4_4 = 3.0f;
 
-        int screenWidth = 400, screenHeight = 400;
+    SetShaderValue(shader, resolutionLoc, res,      UNIFORM_VEC2);
+    SetShaderValue(shader, locationLoc,   location, UNIFORM_VEC2);
+    SetShaderValue(shader, zoomLoc,       (const void*)&zoom,     UNIFORM_FLOAT);
 
-        Color x[screenWidth][screenHeight];
-        int SCALER = 120,scrollSpeed = 4,f = 0,s = 0,zr = 4;
-        bool update = true;
-        InitWindow(screenWidth, screenHeight, "mandlebrot set plotter");
-        SetTargetFPS(60);
-        while (!WindowShouldClose())
+
+    SetShaderValue(shader,  vec4_1Loc, (const void*)&vec4_1,  UNIFORM_FLOAT);
+    SetShaderValue(shader,  vec4_2Loc, (const void*)&vec4_2,  UNIFORM_FLOAT);
+    SetShaderValue(shader,  vec4_3Loc, (const void*)&vec4_3,  UNIFORM_FLOAT);
+    SetShaderValue(shader,  vec4_4Loc, (const void*)&vec4_4,  UNIFORM_FLOAT);
+    SetShaderValue(shader, maxiterLoc, (const void*)&maxIter, UNIFORM_INT);
+
+    // float mousey = 0.0f;
+
+
+#ifdef PLATFORM_ANDROID
+    TexturedButton UI_Button_UP;
+    TexturedButton UI_Button_DOWN;
+    TexturedButton UI_Button_LEFT;
+    TexturedButton UI_Button_RIGHT;
+    TexturedButton UI_Button_PINCH_IN;
+    TexturedButton UI_Button_PINCH_OUT;
+    TexturedButton UI_Button_RESET;
+    
+    UI_Button_UP.texture              = LoadTexture("res/UI_Button_UP.png");
+    UI_Button_DOWN.texture            = LoadTexture("res/UI_Button_DOWN.png");
+    UI_Button_LEFT.texture            = LoadTexture("res/UI_Button_LEFT.png");
+    UI_Button_RIGHT.texture           = LoadTexture("res/UI_Button_RIGHT.png");
+    UI_Button_PINCH_IN.texture        = LoadTexture("res/UI_Button_PINCH_IN.png");
+    UI_Button_PINCH_OUT.texture       = LoadTexture("res/UI_Button_PINCH_OUT.png");
+    UI_Button_RESET.texture           = LoadTexture("res/UI_Button_RESET.png");
+
+
+    int middlex = 1220, middley = 388;
+
+    UI_Button_UP.x = middlex - 50; 
+    UI_Button_UP.y = middley - 150;
+
+    UI_Button_DOWN.x = middlex - 50;
+    UI_Button_DOWN.y = middley + 50;
+    
+    UI_Button_LEFT.x = middlex - 150;
+    UI_Button_LEFT.y = middley - 50;
+
+    UI_Button_RIGHT.x = middlex + 50;
+    UI_Button_RIGHT.y = middley - 50;
+
+    UI_Button_RESET.x = middlex - 50;
+    UI_Button_RESET.y = middley - 50;
+    
+    UI_Button_PINCH_IN.x = 1300;
+    UI_Button_PINCH_IN.y = 590;
+    UI_Button_PINCH_OUT.x = 1300; 
+    UI_Button_PINCH_OUT.y = 450;
+
+
+#endif
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose())
+    {
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText(TextFormat("%d", maxIter), 12, 12, 12, PINK);
+        BeginTextureMode(target);
+            DrawRectangle(0,0,screenWidth, screenHeight, BLACK);
+        EndTextureMode();
+        BeginShaderMode(shader);
+            DrawTexture(target.texture, 0, 0, WHITE);
+        EndShaderMode();
+
+#ifndef PLATFORM_ANDROID
+        maxIter = (int) GuiSliderBar((Rectangle) {screenWidth - 350, 00, 330, 20}, TextFormat("MaxIter: %d", maxIter), NULL, maxIter, 20, 2200);
+        vec4_1 =        GuiSliderBar((Rectangle) {screenWidth - 350, 20, 330, 20}, TextFormat( "vec4_1: %f", vec4_1), NULL,   vec4_1, 0.0f, 5.0f);
+        vec4_2 =        GuiSliderBar((Rectangle) {screenWidth - 350, 40, 330, 20}, TextFormat( "vec4_2: %f", vec4_2), NULL,   vec4_2, 0.0f, 5.0f);
+        vec4_3 =        GuiSliderBar((Rectangle) {screenWidth - 350, 60, 330, 20}, TextFormat( "vec4_3: %f", vec4_3), NULL,   vec4_3, 0.0f, 5.0f);
+        vec4_4 =        GuiSliderBar((Rectangle) {screenWidth - 350, 80, 330, 20}, TextFormat( "vec4_4: %f", vec4_4), NULL,   vec4_4, 0.0f, 5.0f);
+#else
+        maxIter = (int) GuiSliderBar((Rectangle) {screenWidth - 480,  00, 330, 38}, TextFormat("MaxIter: %d", maxIter), NULL, maxIter, 20, 2200);
+        vec4_1 =        GuiSliderBar((Rectangle) {screenWidth - 480,  40, 330, 38}, TextFormat( "vec4_1: %f", vec4_1), NULL,   vec4_1, 0.0f, 5.0f);
+        vec4_2 =        GuiSliderBar((Rectangle) {screenWidth - 480,  80, 330, 38}, TextFormat( "vec4_2: %f", vec4_2), NULL,   vec4_2, 0.0f, 5.0f);
+        vec4_3 =        GuiSliderBar((Rectangle) {screenWidth - 480, 120, 330, 38}, TextFormat( "vec4_3: %f", vec4_3), NULL,   vec4_3, 0.0f, 5.0f);
+        vec4_4 =        GuiSliderBar((Rectangle) {screenWidth - 480, 160, 330, 38}, TextFormat( "vec4_4: %f", vec4_4), NULL,   vec4_4, 0.0f, 5.0f);
+#endif
+
+        DrawText(TextFormat("maxiter: %d, zoom: %f", maxIter, zoom), 12, 12, 10,  RED);
+        DrawText(TextFormat("maxiter: %d, zoom: %f", maxIter, zoom), 12, 22, 10 , BLUE);
+        DrawText(TextFormat("maxiter: %d, zoom: %f", maxIter, zoom), 12, 32, 10 , WHITE);
+#ifdef PLATFORM_ANDROID
+        DrawTexture(UI_Button_RIGHT.texture, UI_Button_RIGHT.x, UI_Button_RIGHT.y, WHITE);                  // UI_Button_RIGHT
+        DrawTexture(UI_Button_LEFT.texture, UI_Button_LEFT.x, UI_Button_LEFT.y, WHITE);                     // UI_Button_LEFT
+        DrawTexture(UI_Button_UP.texture, UI_Button_UP.x, UI_Button_UP.y, WHITE);                           // UI_Button_UP
+        DrawTexture(UI_Button_DOWN.texture, UI_Button_DOWN.x, UI_Button_DOWN.y, WHITE);                     // UI_Button_DOWN
+        DrawTexture(UI_Button_PINCH_IN.texture, UI_Button_PINCH_IN.x, UI_Button_PINCH_IN.y, WHITE);         // UI_Button_PINCH_IN
+        DrawTexture(UI_Button_PINCH_OUT.texture, UI_Button_PINCH_OUT.x, UI_Button_PINCH_OUT.y, WHITE);      // UI_Button_PINCH_OUT
+        DrawTexture(UI_Button_RESET.texture, UI_Button_RESET.x, UI_Button_RESET.y, WHITE);      // UI_Button_PINCH_OUT
+#endif
+        EndDrawing();
+
+        if(GetScreenWidth() != screenWidth || GetScreenHeight() != screenHeight)
         {
-                SCALER += (GetMouseWheelMove()*scrollSpeed);
-                if(SCALER<10) SCALER = 10;
-                if(IsKeyPressed(KEY_R)) {
-                        SCALER = 120;
-                        f = 0;
-                        s = 0;
-                        update = !update;
-                }
-
-
-                if(IsKeyDown(KEY_W)) {
-                        f--;
-                        update = !update;
-                }
-                else if(IsKeyDown(KEY_S)) {
-                        f++;
-                        update = !update;
-                }
-                else if(IsKeyDown(KEY_A)) {
-                        s++;
-                        update = !update;
-                }
-                else if(IsKeyDown(KEY_D)) {
-                        s--;
-                        update = !update;
-                }
-                if(IsKeyPressed(KEY_G)) {
-                        zr++;
-                        update = !update;
-                }
-                else if(IsKeyPressed(KEY_H)) {
-                        zr--;
-                        update = !update;
-                }
-                if(zr<1) zr = 1;
-
-                if(update) {
-                        for(int i=0; i<screenWidth; i++)
-                                for(int j=0; j<screenHeight; j++)
-                                        x[i][j] = mandlebrot(i,j,SCALER,f*MOVE_SPEED,s*MOVE_SPEED,zr);
-                        update = !update;
-                }
-                BeginDrawing();
-                ClearBackground(BLACK);
-                for(int i=0; i<screenWidth; i++)
-                {
-                        for(int j=0; j<screenHeight; j++)
-                        {
-                                DrawPixel(i,j,x[i][j]);
-                        }
-                }
-                DrawRectangle(20,20,20,20,BROWN);
-                DrawText(FormatText("%d",SCALER),23,23,12,RED);
-                EndDrawing();
+            UnloadRenderTexture(target);
+            screenWidth = GetScreenWidth();
+            screenHeight = GetScreenHeight();
+            target = LoadRenderTexture(screenWidth, screenHeight);
         }
-        return 1;
-}
 
+#ifdef PLATFORM_ANDROID
+        if(m_IsButtonPressed(&UI_Button_RIGHT))     location[0] += zoom * 0.019f;
+        if(m_IsButtonPressed(&UI_Button_LEFT))      location[0] -= zoom * 0.019f;
+        if(m_IsButtonPressed(&UI_Button_UP))        location[1] += zoom * 0.019f;
+        if(m_IsButtonPressed(&UI_Button_DOWN))      location[1] -= zoom * 0.019f;
+        if(m_IsButtonPressed(&UI_Button_PINCH_IN))  zoom += zoom * 0.04f;
+        if(m_IsButtonPressed(&UI_Button_PINCH_OUT)) zoom -= zoom * 0.04f;
+#else
+        if(IsKeyDown(KEY_RIGHT))                   location[0] -= zoom * 0.05f;
+        if(IsKeyDown(KEY_LEFT))                    location[0] += zoom * 0.05f;
+        if(IsKeyDown(KEY_UP))                      location[1] -= zoom * 0.05f;
+        if(IsKeyDown(KEY_DOWN))                    location[1] += zoom * 0.05f;
+        if(IsKeyDown(KEY_S))                       zoom += zoom * 0.01f;
+        if(IsKeyDown(KEY_W))                       zoom -= zoom * 0.01f;
+#endif
 
-Color mandlebrot(int x, int y,int scl,int fr, int si,int zrn)
-{
-        complex c = { x, y }, z = { 0, 0 };
-        c = maptograph(c, scl, fr, si);
-        for(int iter = 1; iter <= MAXITER; iter++)
+//         start_game_button = GuiButton((Rectangle){ GetScreenWidth() / 3 + 100,GetScreenHeight() / 3 + 125,250,100 },"");
+
+#ifdef PLATFORM_ANDROID
+        if(m_IsButtonPressed(&UI_Button_RESET))
+#else
+            // break;
+        if(IsKeyDown(KEY_R))
+#endif
         {
-                z = compadd(compsquare(z),c);
-                if(compabs(z)>zrn)
-                {
-                        switch(iter)
-                        {
-                        case 1:
-                                return BLUE;
-                        case 2:
-                                return WHITE;
-                        case 3:
-                                return ORANGE;
-                        case 4:
-                                return RED;
-                        case 5:
-                                return DARKPURPLE;
-                        case 6:
-                                return PINK;
-                        case 7:
-                                return DARKGRAY;
-                        case 8:
-                                return DARKBROWN;
-                        case 9:
-                                return DARKBLUE;
-                        case 10:
-                                return PURPLE;
-                        case 11:
-                                return BEIGE;
-                        case 12:
-                                return SKYBLUE;
-                        case 13:
-                                return BLACK;
-                        case 14:
-                                return MAGENTA;
-                        case 15:
-                                return BROWN;
-                        case 16:
-                                return LIGHTGRAY;
-                        case 17:
-                                return GOLD;
-                        case 18:
-                                return ORANGE;
-                        case 19:
-                                return DARKGREEN;
-                        case 20:
-                                return LIME;
-                        case 21:
-                                return BLUE;
-                        }
-                        return VIOLET;
-                }
-        }
-        return BLACK;
-}
-
-int compabs(complex cx){
-        int x;
-        x = (cx.real*cx.real)+(cx.imag*cx.imag);
-        return x;
-}
-
-complex compadd(complex c_1,complex c_2)
-{
-        complex cr;
-        cr.real = c_1.real+c_2.real;
-        cr.imag = c_1.imag+c_2.imag;
-        return cr;
-}
-
-
-float splitcomponents(complex x,int COMP)
-{
-        if(COMP == REAL)
-        {
-                return x.real;
-        } else {
-                return x.imag;
+            location[0] = 0.0;          
+            location[1] = 0.0;
+            zoom = 2.0;
+            maxIter = 256;
+            vec4_1 = 1.0f;
+            vec4_2 = 2.0f / 3.0f;
+            vec4_3 = 1.0f / 3.0f;
+            vec4_4 = 3.0f;
         }
 
+
+        SetShaderValue(shader, resolutionLoc,               res,        UNIFORM_VEC2);
+        SetShaderValue(shader, locationLoc,                 location,   UNIFORM_VEC2);
+        SetShaderValue(shader, zoomLoc,       (const void*)&zoom,       UNIFORM_FLOAT);
+        SetShaderValue(shader, vec4_1Loc,     (const void*)&vec4_1,     UNIFORM_FLOAT);
+        SetShaderValue(shader, vec4_2Loc,     (const void*)&vec4_2,     UNIFORM_FLOAT);
+        SetShaderValue(shader, vec4_3Loc,     (const void*)&vec4_3,     UNIFORM_FLOAT);
+        SetShaderValue(shader, vec4_4Loc,     (const void*)&vec4_4,     UNIFORM_FLOAT);
+        SetShaderValue(shader, maxiterLoc,    (const void*)&maxIter,    UNIFORM_INT);
+        if(IsKeyPressed(KEY_BACK))
+            break;
+    }
+    
+    UnloadShader(shader);
+    UnloadRenderTexture(target);
+#ifdef PLATFORM_ANDROID
+    UnloadTexture(UI_Button_RIGHT.texture);
+    UnloadTexture(UI_Button_LEFT.texture);
+    UnloadTexture(UI_Button_UP.texture);
+    UnloadTexture(UI_Button_DOWN.texture);
+    UnloadTexture(UI_Button_PINCH_IN.texture);
+    UnloadTexture(UI_Button_PINCH_OUT.texture);
+    UnloadTexture(UI_Button_RESET.texture);
+#endif
+
+
+    CloseWindow();
+    exit(EXIT_SUCCESS);
+    return 0;
 }
 
 
-complex maptograph(complex x,int sc, int frn, int sid)
+bool m_IsButtonPressed(TexturedButton* texbutton)
 {
-
-        complex z;
-        z.real = (x.real - (GetScreenWidth()/2) - sid)/sc;
-        z.imag = ((GetScreenHeight()/2) - x.imag - frn)/sc;
-        return z;
-}
-
-/*
-   complex maptoscreen(complex x)
-   {
-        complex z;
-        z.real = (GetScreenWidth()/2) + (x.real*SCALER);
-        z.imag = (GetScreenHeight()/2) - (x.imag*SCALER);
-        return z;
-   }
-
- */
-complex compsquare(complex x)
-{
-        complex z;
-        z.real = (x.real*x.real) - (x.imag*x.imag);
-        z.imag = 2*x.real*x.imag;
-        return z;
+    // int x = GetMouseX(), y = GetMouseY();
+    Vector2 touch = GetTouchPosition(0);
+    if(IsGestureDetected(GESTURE_HOLD))
+        if(touch.x > texbutton->x && touch.y > texbutton->y && touch.x < (texbutton->x + texbutton->texture.width) && touch.y < (texbutton->y + texbutton->texture.width))
+            return true;
+    // if(IsGestureDetected(GESTURE_HOLD))
+    //  if( touch.x > texbutton->x &&  touch.y > texbutton->y )
+    //      if( touch.x < (texbutton->x + texbutton->x + texbutton->texture.width ) &&
+    //          touch.y < (texbutton->y + texbutton->y + texbutton->texture.height))
+                // return true;
+    return false;
 }
